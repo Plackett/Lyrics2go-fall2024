6983eef1a948539754a295bb56b5cffaac3e544a
Plackett <johnnicholasdimitri@gmail.com>
2024-10-21 00:32:53 -0400

    added new page

---
+++ app/(tabs)/lyrics.tsx
@@ -0,0 +1,11 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ScrollView } from "react-native";
+
+export default function ChrisScreen() {
+    return (
+        <ScrollView>
+            <ThemedText> </ThemedText>
+            <ThemedText>  best friends together forever!</ThemedText>
+        </ScrollView>
+    );
+}

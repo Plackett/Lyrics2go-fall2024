87839ad8962b652a71f05d7cff002dd2a922041a
Colin L <eternalnova685@gmail.com>
2024-11-06 19:32:13 -0500

    started the lyrics page for the handleSubmit function to use

---
+++ app/(tabs)/lyrics.tsx
@@ -0,0 +1,9 @@
+import { Text, TextInput, View } from 'react-native';
+
+// i wanna add a parameter into this function to make the lyrics for a specific song show up;
+// i also need the way the backend will be implemented to do this in an effective way tbh
+export default function lyrics() {
+    return (
+        <text> </text>
+    )
+}
